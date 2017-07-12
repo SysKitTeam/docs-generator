449b53166ac2c384e324e9ae2875a2421c554c58
Ivan Cottier <ivan.cottiero@fer.hr>
2017-07-12 14:50:33 +0200

    Filled some methods for generating pdf

--- DocsGenerator/DocsGenerator/HtmlToPdfParser.cs
+++ DocsGenerator/DocsGenerator/HtmlToPdfParser.cs
@@ -10,10 +10,34 @@ namespace DocsGenerator
     {
         public static bool GeneratePdf(ref List<DocumentsWrapper> docsList, string outputPath)
         {
-
+            // First, generate multiple pdfs from all the html files
+            generateIndividualPdfs(ref docsList);
+            // Second, gather all generated pdfs to a single one:
             return true;
         }
 
-        
+        private static bool generateIndividualPdfs(ref List<DocumentsWrapper> docsList)
+        {
+            for(int i = 0; i < docsList.Count; i++)
+            {
+                DocumentsWrapper doc = docsList[i];
+                if (doc.IsDirectory)
+                {
+                    List<DocumentsWrapper> newDocsList = doc.SubDocuments;
+                    if (!generateIndividualPdfs(ref newDocsList)) return false;
+                    doc.SubDocuments = newDocsList;
+                }
+                else
+                {
+                    if (!toPdf(ref doc)) return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool toPdf(ref DocumentsWrapper doc)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
