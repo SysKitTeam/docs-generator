42557cb9e0315cbd5144bd7145961093a8aca9e2
Ivan Cottier <ivan.cottiero@fer.hr>
2017-07-12 12:55:57 +0200

    DocumentsWrapper - added html path file generator MdToHtml - filled empty methods. Not yet tested

--- DocsGenerator/DocsGenerator/DocumentsWrapper.cs
+++ DocsGenerator/DocsGenerator/DocumentsWrapper.cs
@@ -19,5 +19,22 @@ namespace DocsGenerator
         public string HtmlPath { get; set; }
         public bool IsDirectory { get; set; }
         public List<DocumentsWrapper> SubDocuments { get; set; }
+        /// <summary>
+        /// Generates a path for a html file from GitPath. Path also includes the correct file name
+        /// with a .html extension
+        /// </summary>
+        /// <param name="setInternally">If true, given value is also stored to HtmlPath property.</param>
+        /// <returns>Path for html file.</returns>
+        public string GenerateHtmlPath(bool setInternally = false)
+        {
+            if (String.IsNullOrEmpty(GitPath)) return string.Empty;
+            int i = GitPath.LastIndexOf('\\');
+            if (!IsDirectory)
+            {
+                string shortFileName = fileName.Substring(0, fileName.LastIndexOf('.'));
+                return GitPath.Substring(0, i + 1) + shortFileName;
+            }
+            else return GitPath.Substring(0, i + 1) + fileName;
+        }
     }
 }
--- DocsGenerator/DocsGenerator/MdToHtmlParser.cs
+++ DocsGenerator/DocsGenerator/MdToHtmlParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,9 +27,51 @@ namespace DocsGenerator
             return true;
         }
 
+        /// <summary>
+        /// Prepares md file for conversion to html. This method will overwrite the old md file.
+        /// </summary>
+        /// <param name="path">Path of the md file to be edited.</param>
         public static void editMdFile(string path)
         {
+            string tempFile = Path.GetTempFileName();
 
+            using (var reader = new StreamReader(path))
+            using (var writer = new StreamWriter(tempFile))
+            {
+                int headerLinesCount = 0;
+                bool inHeader = false;
+                string line;
+                while((line = reader.ReadLine()) != null)
+                {
+                    // header:
+                    if (line.StartsWith("---"))
+                    {
+                        headerLinesCount++;
+                        if (headerLinesCount < 2)
+                        {
+                            inHeader = true;
+                        } else
+                        {
+                            inHeader = false;
+                        }
+                        writer.WriteLine(line);
+                    }
+                    else if (inHeader)
+                    {
+                        if (line.StartsWith("title:"))
+                        {
+                            writer.WriteLine(line.Substring(6));
+                        }
+                    }
+                    else // other:
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+
+            File.Delete(path);
+            File.Move(tempFile, path);
         }
 
         private static void parseRecursive(ref DocumentsWrapper doc)
@@ -46,7 +89,11 @@ namespace DocsGenerator
 
         private static void parseFileToHtml(ref DocumentsWrapper doc)
         {
-
+            using (var reader = new System.IO.StreamReader(doc.GitPath))
+            using (var writer = new System.IO.StreamWriter(doc.GenerateHtmlPath()))
+            {
+                CommonMark.CommonMarkConverter.Convert(reader, writer);
+            }
         }
     }
 }
