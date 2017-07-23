8982d76398a6951d2caaaa842efb2d1fc72ed12b
Ivan Cottier <ivan.cottiero@fer.hr>
2017-07-23 15:19:30 +0200

    Added cover, headeer and footer html files to pdf generation.

--- DocsGenerator/DocsGenerator/HtmlToPdfParser.cs
+++ DocsGenerator/DocsGenerator/HtmlToPdfParser.cs
@@ -10,13 +10,26 @@ namespace DocsGenerator
 {
     class HtmlToPdfParser
     {
+        /// <summary>
+        /// Generates a single pdf file with default header, footer and cover, along with the default css file.
+        /// </summary>
+        /// <param name="docsList">Recursive list of documents that will be in the pdf document.</param>
+        /// <param name="outputPath">Full path of the pdf to be created.</param>
+        /// <param name="tmpDirPath">Path of a temporary directory for mid-calculation file storage.</param>
+        /// <param name="documentTitle">Title of the document to be created.</param>
+        /// <returns>True if pdf generation was successfull, false otherwise.</returns>
         public bool GeneratePdf(List<DocumentsWrapper> docsList, string outputPath, string tmpDirPath, string documentTitle)
         {
             string tmpFile = tmpDirPath + "ALL.html";
+
             copyCSSfile(tmpDirPath);
+            string headerPath = copyHeaderFile(tmpDirPath);
+            string footerPath = copyFooterFile(tmpDirPath);
+            string coverPath = copyAndEditCoverFile(tmpDirPath, documentTitle, DateTime.Now);
+
             generateSingleHtmlFile(docsList, tmpFile);
-            toPdf(tmpFile, outputPath, documentTitle);
-            return true;
+            
+            return toPdf(tmpFile, outputPath, headerPath, footerPath, coverPath); ;
         }
 
         public void generateSingleHtmlFile(List<DocumentsWrapper> docsList, string outputFile)
@@ -33,12 +46,75 @@ namespace DocsGenerator
             
         }
 
-        private void copyCSSfile(string outputPath)
+        /// <summary>
+        /// Creates a copy of project style.css file to the given output path.
+        /// </summary>
+        /// <param name="outputPath">Destination directory path.</param>
+        /// <returns>Full file path of the new style.css file.</returns>
+        private string copyCSSfile(string outputPath)
         {
             File.Copy(".\\style.css", outputPath + "style.css");
+            return outputPath + "style.css";
         }
 
-        private void appendText(string inputFile, StreamWriter outputWriter, int level)
+        /// <summary>
+        /// Creates a copy of project header.html file to the given output path.
+        /// </summary>
+        /// <param name="outputPath">Destination directory path.</param>
+        /// <returns>Full file path of the new header.html file.</returns>
+        private string copyHeaderFile(string outputPath)
+        {
+            File.Copy(".\\header.html", outputPath + "header.html");
+            return outputPath + "header.html";
+        }
+
+        /// <summary>
+        /// Creates a copy of project footer.html file to the given output path.
+        /// </summary>
+        /// <param name="outputPath">Destination directory path.</param>
+        /// <returns>Full file path of the new footer.html file.</returns>
+        private string copyFooterFile(string outputPath)
+        {
+            File.Copy(".\\footer.html", outputPath + "footer.html");
+            return outputPath + "footer.html";
+        }
+
+        /// <summary>
+        /// Creates a copy of project cover.html file with document data to the given output path.
+        /// </summary>
+        /// <param name="outputPath">Destination directory path.</param>
+        /// <param name="documentTitle">Title of the document that will be put in the cover file.</param>
+        /// <param name="dateTime">Date parameter that will be put in the cover file.</param>
+        /// <returns>Full file path of the new cover.html file.</returns>
+        private string copyAndEditCoverFile(string outputPath, string documentTitle, DateTime dateTime)
+        {
+            using (StreamReader reader = new StreamReader(".\\cover.html"))
+            using (StreamWriter writer = new StreamWriter(outputPath + "cover.html"))
+            {
+                string line;
+                while((line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains("_title_"))
+                    {
+                        line = line.Replace("_title_", documentTitle);
+                    }
+                    if (line.Contains("_date_"))
+                    {
+                        line = line.Replace("_date_", dateTime.ToShortDateString());
+                    }
+                    writer.WriteLine(line);
+                }
+            }
+                
+            return outputPath + "cover.html";
+        }
+
+        /// <summary>
+        /// Copies all contents of the given file to the outputWriter stream
+        /// </summary>
+        /// <param name="inputFile">Path of the file that will be copied.</param>
+        /// <param name="outputWriter">Output stream for copying contents.</param>
+        private void appendText(string inputFile, StreamWriter outputWriter)
         {
             using (StreamReader reader = new StreamReader(inputFile))
             {
@@ -50,6 +126,13 @@ namespace DocsGenerator
             }
         }
 
+        /// <summary>
+        /// Writes a header html line to the output streamWriter with a document title taken from the given document.
+        /// This is used for directories (titles with no content).
+        /// </summary>
+        /// <param name="doc">Document from which the title will be taken.</param>
+        /// <param name="writer">Output stream where title will be written.</param>
+        /// <param name="level">Header level.</param>
         private void appendDirectoryTitle(DocumentsWrapper doc, StreamWriter writer, int level)
         {
             string line = "<h" + level + "><font face=\"Arial\" size=\"10\" color=\"#eca11d\">" + doc.Title + "</font></h" + level + ">";
@@ -62,7 +145,7 @@ namespace DocsGenerator
             {
                 if (!doc.IsDirectory)
                 {
-                    appendText(doc.HtmlPath, writer, level);
+                    appendText(doc.HtmlPath, writer);
                 }
                 else
                 {
@@ -72,14 +155,22 @@ namespace DocsGenerator
             }
         }
 
-        private bool toPdf(string inputHtmlPath, string outputPdfPath, string documentTitle)
+        /// <summary>
+        /// Generates a pdf using wkhtmltopdf with given content, header, footer and cover files to the given output path.
+        /// </summary>
+        /// <param name="inputHtmlPath">Path of the html file with contents.</param>
+        /// <param name="outputPdfPath">Path where pdf file will be created.</param>
+        /// <param name="headerPath">Path of the header html file.</param>
+        /// <param name="footerPath">Path of the footer html file.</param>
+        /// <param name="coverPath">Path of the cover html file.</param>
+        /// <returns>True if pdf generation was successfull, false otherwise.</returns>
+        private bool toPdf(string inputHtmlPath, string outputPdfPath, string headerPath, string footerPath, string coverPath)
         {
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = "wkhtmltopdf.exe";
-            startInfo.Arguments = "--header-left " + documentTitle + " " +
-                                  "--header-right [page]/[topage] " +
-                                  "--header-line " +
-                                  "--header-spacing 2 " +
+            startInfo.Arguments = "--header-html " + headerPath + " " +
+                                  "--header-spacing " + footerPath + " " +
+                                  "cover " + coverPath + " " +
                                   "toc " + 
                                   inputHtmlPath + " " + 
                                   outputPdfPath;
